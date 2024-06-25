3c87298e2b8363aa6a6ea8c2256ca3cb22313586
NokoDev <elvisnoko18@gmail.com>
2024-06-25 15:34:46 +0200

    Add Hero component with styles for main introductory section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,11 @@
 import styles from "./App.module.css";
+import { Hero } from "./Components/Hero/Hero";
 import { Navbar } from "./Components/Navbar/navbar";
 
 function App() {
   return <div className={styles.App}>
     <Navbar/>
+    <Hero/>
   </div>;
 }
 
---
+++ Portfolio/src/Components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm NokoDev</h1>
+        <p className={styles.description}>
+          I'm a software developer with 3 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:elvisnoko18@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/hero.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
