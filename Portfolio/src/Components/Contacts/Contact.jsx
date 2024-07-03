e2caf14b059b65096045259dab530da188cbff66
NokoDev <elvisnoko18@gmail.com>
2024-07-03 15:45:59 +0200

    Updated

--- Portfolio/src/Components/Contacts/Contact.jsx
+++ Portfolio/src/Components/Contacts/Contact.jsx
@@ -5,29 +5,29 @@ import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
-  return (
-    <footer id="contact" className={styles.container}>
-      <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
-      </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
-        </li>
-      </ul>
-    </footer>
-  );
+    return (
+        <footer id="contact" className={styles.container}>
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                    <a href="mailto:elvisnoko18@email.com">elvisnoko18@email.com</a>
+                </li>
+                <li className={styles.link}>
+                    <img
+                        src={getImageUrl("contact/linkedinIcon.png")}
+                        alt="LinkedIn icon"
+                    />
+                    <a href="https://linkedin.com/in/nokodev/">https://linkedin.com/in/nokodev/</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+                    <a href="https://github.com/esnoko">github.com/esnoko</a>
+                </li>
+            </ul>
+        </footer>
+    );
 };
